a5c66d24191b63e1561048eb3dd507a8439b5ab7
App Academy Student <student@appacademy.io>
2019-05-17 16:26:59 -0700

    Implement PolyTreeNode

--- skeleton/lib/00_tree_node.rb
+++ skeleton/lib/00_tree_node.rb
@@ -12,16 +12,31 @@ class PolyTreeNode
     @parent = nil
   end
 
-  def parent =(mom)
-    @parent = mom
-    # debugger 
-    mom.children << self
+  def parent=(new_parent)
+    unless @parent.nil? #check to see if old parent exist and makes old parent forget child
+        @parent.children.reject! {|children| children == self} 
+    end
+
+    if new_parent.nil? 
+      @parent = nil
+    else
+      @parent = new_parent
+      new_parent.children << self
+    end
+  end
+
+  def add_child(child)
+    child.parent = self
+  end
+
+  def remove_child(child)
+    raise "That is not their child" unless @children.include?(child)
+    child.parent = nil
   end
 
   def inspect
     "<PolyTreeNode V=#{value} parent= #{parent} children= #{children}>"
   end
-
 
 end
 
@@ -33,9 +48,9 @@ def build_extree
   p5 = PolyTreeNode.new(5)
 
   p2.parent = p1
-  p3.parent = p1
-  p4.parent = p2
-  p5.parent = p2
+  # p3.parent = p1
+  # p4.parent = p2
+  # p5.parent = p2
 
   p p1
   p p2
@@ -43,4 +58,4 @@ def build_extree
   p p2.parent.children
 end
 
-build_extree
+# build_extree
